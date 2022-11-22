91655fb5fd287e6977de21e9cd9680585e512777
Tymek <tymek.morawiec@gmail.com>
2022-11-22 16:54:02 +0100

    added mechanics to go between pages and updated offer page

--- intersectionObserver.js
+++ intersectionObserver.js
@@ -1,18 +1,1 @@
-const medical_list = document.querySelectorAll(".medicals__token");
-const offer_token = document.querySelectorAll(".our__offer");
-const observerOne = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("medicals__token--visible");
-    }
-  });
-});
-const observerTwo = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("our__offer--visible");
-    }
-  });
-});
-medical_list.forEach((element) => observerOne.observe(element));
-offer_token.forEach((element) => observerTwo.observe(element));
+
--- offer.js
+++ offer.js
@@ -2,11 +2,63 @@ const ourOffer = document.getElementById("our__offer");
 const Sections = document.querySelectorAll(
   ".default__section__one, .default__section__two"
 );
-const offerSection = document.getElementById("sectionOffer");
-const ourOfferHandler = () => {
-  Sections.forEach((section) => {
-    section.classList.toggle("not__visible");
+// intersection observer
+const medical_list = document.querySelectorAll(".medicals__token");
+const offer_token = document.querySelectorAll(".our__offer");
+const observerOne = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("medicals__token--visible");
+    }
   });
-  offerSection.classList.toggle("not__visible");
+});
+const observerTwo = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("our__offer--visible");
+    }
+  });
+});
+medical_list.forEach((element) => observerOne.observe(element));
+offer_token.forEach((element) => observerTwo.observe(element));
+//-----------------------------------------------------------------------
+
+const mainPage = document.getElementById("mainPage");
+const offerPage = document.getElementById("offerPage");
+const mainContainer = document.querySelector("main");
+
+const renderOfferPage = () => {
+  removeChildNodes(mainContainer);
+  const cloneOfferPage = offerPage.content.cloneNode(true);
+  const gobackButton = document.createElement("button");
+  gobackButton.textContent = "GO BACK";
+  gobackButton.addEventListener("click", renderMainPage);
+  document.body.dataset.page = "offer";
+  mainContainer.appendChild(cloneOfferPage);
+  mainContainer.appendChild(gobackButton);
+  window.scrollTo(0, 0);
 };
-ourOffer.addEventListener("click", ourOfferHandler);
+const renderMainPage = () => {
+  removeChildNodes(mainContainer);
+  const cloneMainPage = mainPage.content.cloneNode(true);
+  mainContainer.appendChild(cloneMainPage);
+  mainContainer
+    .querySelectorAll(".medicals__token")
+    .forEach((element) => observerOne.observe(element));
+  mainContainer
+    .querySelectorAll(".our__offer")
+    .forEach((element) => observerTwo.observe(element));
+  mainContainer
+    .querySelector(".our__offer")
+    .addEventListener("click", renderOfferPage);
+  window.scrollTo(0, 0);
+};
+const removeChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+};
+
+ourOffer.addEventListener("click", () => {
+  renderOfferPage();
+});
